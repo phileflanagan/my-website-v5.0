e3d95ea3bb2da531fc7a566e9cd08fa2dfbd39d7
Phil Flanagan <phileflanagan@gmail.com>
2016-10-17 12:27:16 -0400

    Adjust animation timing and remove mobile menu on link click

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 $(function() {
-  
+
   // Smooth Scroll
   $('a[href*="#"]:not([href="#"])').click(function() {
      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
        var target = $(this.hash);
        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
        if (target.length) {
+         $('#nav-toggle-cbox').prop('checked', false);
          $('html, body').animate({
            scrollTop: target.offset().top
          }, 500);
