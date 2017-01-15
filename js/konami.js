b80f93d20b96e3a164677f0a3d27eee751797db4
Phil Flanagan <phileflanagan@gmail.com>
2017-01-15 15:02:07 -0500

    Add new project

--- js/konami.js
+++ js/konami.js
@@ -6,10 +6,13 @@ const konamiDiv = document.querySelector('.konami');
 const konamiClose = konamiDiv.querySelector('.img-wrap');
 konamiClose.addEventListener('click', () => konamiDiv.style.display = 'none')
 
+
 window.addEventListener('keyup', (e) => {
+  console.time('start func')
   pressed.push(e.key);
   pressed.splice(-konami.length - 1, pressed.length - konami.length);
   if(konami.every((v,i)=> v === pressed[i])) {
     konamiDiv.style.display = 'flex';
+    console.timeEnd('start func');
   }
 });
